bf4664140653eb8c4c71abe8244727ba35ab9f45
D <s90789@outlook.com>
2018-07-02 23:54:11 +0800

    Add LayoutParams

--- lib/src/main/java/com/d/lib/slidelayout/SlideLayout.java
+++ lib/src/main/java/com/d/lib/slidelayout/SlideLayout.java
@@ -2,6 +2,8 @@ package com.d.lib.slidelayout;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.view.MotionEvent;
@@ -9,6 +11,8 @@ import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.Scroller;
+
+import java.util.ArrayList;
 
 /**
  * SlideLayout
@@ -18,6 +22,7 @@ public class SlideLayout extends ViewGroup {
     private int width;
     private int height;
 
+    private final ArrayList<View> mMatchParentChildren = new ArrayList<>(1);
     private Scroller scroller;
     private int leftBorder;
     private int rightBorder;
@@ -58,17 +63,133 @@ public class SlideLayout extends ViewGroup {
     }
 
     @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        width = MeasureSpec.getSize(widthMeasureSpec);
-        height = MeasureSpec.getSize(heightMeasureSpec);
-        int count = getChildCount();
-        for (int i = 0; i < count; i++) {
-            View child = getChildAt(i);
-            //为ViewGroup中的每一个子控件测量大小
-            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new LayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
+        if (false) {
+            if (lp instanceof LayoutParams) {
+                return new LayoutParams((LayoutParams) lp);
+            } else if (lp instanceof MarginLayoutParams) {
+                return new LayoutParams((MarginLayoutParams) lp);
+            }
         }
-        setMeasuredDimension(width, height);
+        return new LayoutParams(lp);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
+        return new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int count = getChildCount();
+
+        final boolean measureMatchParentChildren = MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.EXACTLY ||
+                MeasureSpec.getMode(heightMeasureSpec) != MeasureSpec.EXACTLY;
+
+        if (!measureMatchParentChildren) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            width = MeasureSpec.getSize(widthMeasureSpec);
+            height = MeasureSpec.getSize(heightMeasureSpec);
+            for (int i = 0; i < count; i++) {
+                View child = getChildAt(i);
+                //为ViewGroup中的每一个子控件测量大小
+                measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            }
+            setMeasuredDimension(width, height);
+            return;
+        }
+
+        mMatchParentChildren.clear();
+
+        int maxHeight = 0;
+        int maxWidth = 0;
+        int childState = 0;
+
+        for (int i = 0; i < count; i++) {
+            final View child = getChildAt(i);
+            if (child.getVisibility() != GONE) {
+                measureChildWithMargins(child, widthMeasureSpec, 0, heightMeasureSpec, 0);
+                final SlideLayout.LayoutParams lp = (SlideLayout.LayoutParams) child.getLayoutParams();
+                maxWidth = Math.max(maxWidth,
+                        child.getMeasuredWidth() + lp.leftMargin + lp.rightMargin);
+                maxHeight = Math.max(maxHeight,
+                        child.getMeasuredHeight() + lp.topMargin + lp.bottomMargin);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+                    childState = combineMeasuredStates(childState, child.getMeasuredState());
+                }
+
+                if (measureMatchParentChildren) {
+                    if (lp.width == SlideLayout.LayoutParams.MATCH_PARENT ||
+                            lp.height == SlideLayout.LayoutParams.MATCH_PARENT) {
+                        mMatchParentChildren.add(child);
+                    }
+                }
+            }
+        }
+
+        // Check against our minimum height and width
+        maxHeight = Math.max(maxHeight, getSuggestedMinimumHeight());
+        maxWidth = Math.max(maxWidth, getSuggestedMinimumWidth());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Check against our foreground's minimum height and width
+            final Drawable drawable = getForeground();
+            if (drawable != null) {
+                maxHeight = Math.max(maxHeight, drawable.getMinimumHeight());
+                maxWidth = Math.max(maxWidth, drawable.getMinimumWidth());
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            setMeasuredDimension(resolveSizeAndState(maxWidth, widthMeasureSpec, childState),
+                    resolveSizeAndState(maxHeight, heightMeasureSpec,
+                            childState << MEASURED_HEIGHT_STATE_SHIFT));
+        } else {
+            setMeasuredDimension(width, height);
+        }
+
+        count = mMatchParentChildren.size();
+        if (count > 1) {
+            for (int i = 0; i < count; i++) {
+                final View child = mMatchParentChildren.get(i);
+                final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+
+                final int childWidthMeasureSpec;
+                if (lp.width == SlideLayout.LayoutParams.MATCH_PARENT) {
+                    final int width = Math.max(0, getMeasuredWidth()
+                            - lp.leftMargin - lp.rightMargin);
+                    childWidthMeasureSpec = MeasureSpec.makeMeasureSpec(
+                            width, MeasureSpec.EXACTLY);
+                } else {
+                    childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec,
+                            lp.leftMargin + lp.rightMargin,
+                            lp.width);
+                }
+
+                final int childHeightMeasureSpec;
+                if (lp.height == SlideLayout.LayoutParams.MATCH_PARENT) {
+                    final int height = Math.max(0, getMeasuredHeight()
+                            - lp.topMargin - lp.bottomMargin);
+                    childHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+                            height, MeasureSpec.EXACTLY);
+                } else {
+                    childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec,
+                            lp.topMargin + lp.bottomMargin,
+                            lp.height);
+                }
+
+                child.measure(childWidthMeasureSpec, childHeightMeasureSpec);
+            }
+        }
+
+        width = getMeasuredWidth();
+        height = getMeasuredHeight();
     }
 
     @Override
@@ -219,13 +340,32 @@ public class SlideLayout extends ViewGroup {
         /**
          * 关闭所有未关闭的Slide
          *
-         * @param layout;this
-         * @return true:存在未关闭Slide;false:不存在未关闭Slide
+         * @param layout this
+         * @return true:存在未关闭Slide; false:不存在未关闭Slide
          */
         boolean closeAll(SlideLayout layout);
     }
 
     public void setOnStateChangeListener(OnStateChangeListener listener) {
         this.listener = listener;
+    }
+
+    public static class LayoutParams extends MarginLayoutParams {
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams source) {
+            super(source);
+        }
     }
 }
